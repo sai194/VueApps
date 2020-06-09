5e5768c1ff6f31d2c2d2dc59453faa5dad9eac0c
Sai Yeluri <sai194@gmail.com>
2020-06-09 14:38:54 -0500

    s
    s

---
+++ bootstrapvue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <HelloWorld/>
+  </div>
+</template>
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+  import Navbar from './components/Navbar.vue';
+  export default {
+    name: 'App',
+    components: {
+      Navbar,
+      HelloWorld
+    }
+  }
+</script>
